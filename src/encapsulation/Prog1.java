3bf2c6e0577e5eb39b7a192d47fa21b9439caa66
unknown <AsifRS>
2022-11-14 16:18:58 +0530

    first commit

---
+++ src/encapsulation/Prog1.java
@@ -0,0 +1,18 @@
+	package encapsulation;
+
+	public class Prog1 {
+
+	private int empid = 100;
+	private String name="Asif Shaikh";
+	private double salary= 95000.20;
+	
+	
+	public void empinfo() {
+		
+		System.out.println(" Employee id ="+empid);
+		System.out.println(" Employee name ="+name);
+		System.out.println(" Employee salary ="+salary);
+	
+		
+	}
+	}
---
+++ src/encapsulation/Prog1Impliment.java
@@ -0,0 +1,11 @@
+package encapsulation;
+
+public class Prog1Impliment {
+
+	public static void main(String[] args) {
+
+		Prog1 a =new Prog1();
+		a.empinfo();
+	}
+
+}
